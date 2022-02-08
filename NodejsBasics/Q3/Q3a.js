abbef3749029c6508be6b2e2941c04cf91dc4759
Revanth challa <98075948+Revanthchalla@users.noreply.github.com>
2022-02-09 00:31:17 +0530

    Update Q3a.js

--- NodejsBasics/Q3/Q3a.js
+++ NodejsBasics/Q3/Q3a.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = 'http://www.google.com';
 request(url, (err,res,body)=>{
     if(err) return err
-    fs.writeFile('data3.html', body, 'utf8', (err)=>{
+    fs.writeFile('data.html', body, 'utf8', (err)=>{
         if(err) return err;
         console.log('data pulled and created file successfully')
     })
